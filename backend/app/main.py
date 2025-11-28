a72e072d1ff5103d0b7079f12bea003ff8acec1c
Manuel-Correderas <112085585+Manuel-Correderas@users.noreply.github.com>
2025-11-28 19:17:12 -0300

    cambios básicos

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,11 @@
 # backend/app/main.py
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from backend.app.routers import auth
 
-from .db import Base, engine, init_db
+
+from .db import init_db
+
 from .routers import (
     routes_analytics,
     routes_products,
@@ -12,9 +14,9 @@ from .routers import (
     routes_roles,
     routes_product_comments,
     routes_orders,
-    routes_comments,
+    routes_comments,   
     routes_auth,
-    routes_cart,
+    routes_cart,  
     routes_admin,
     routes_sales,
     routes_premium,
@@ -22,56 +24,42 @@ from .routers import (
 
 app = FastAPI(title="Ecom MKT Lab API")
 
-# =========================
-# CORS
-# =========================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:8501",
-        "http://127.0.0.1:8501",
-        "https://devops-tpi-final-hastesters.onrender.com",
-        "https://tpidevopsfrontend-mvrp7p6g5gxjyqsrxcfbt5.streamlit.app",
-    ],
+    allow_origins=["http://localhost:8501", "http://127.0.0.1:8501"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 
-# =========================
-# HEALTHCHECK
-# =========================
 @app.get("/health")
 def health():
     return {"ok": True}
 
 
-# =========================
-# STARTUP
-# =========================
 @app.on_event("startup")
 def on_startup():
-    """
-    Registra los modelos y crea tablas si no existen.
-    init_db() ya se encarga de importar models y hacer create_all.
-    """
     init_db()
 
 
-# =========================
-# ROUTERS
-# =========================
+# Routers
 app.include_router(routes_roles.router)
 app.include_router(routes_users.router)
 app.include_router(routes_products.router)
 app.include_router(routes_product_comments.router)
 app.include_router(routes_orders.router)
-app.include_router(routes_comments.router)
-app.include_router(routes_auth.router)
-app.include_router(routes_cart.router)
-app.include_router(routes_admin.router)
+app.include_router(routes_comments.router)  
+app.include_router(routes_auth.router)     
+app.include_router(routes_cart.router)  
+app.include_router(routes_admin.router)  # 👈 NUEVO
+
 app.include_router(routes_sales.router)
+
 app.include_router(routes_order_items.router)
+
 app.include_router(routes_analytics.router)
 app.include_router(routes_premium.router)
+
+
+app.include_router(auth.router) 
